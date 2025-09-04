3c8250ef2817f315ba604dc3076d43548bdfd1c9
nostalgiaCode <nowelywork@gmail.com>
2025-09-04 16:15:40 +0300

    authorization

--- credentials/GreenApiAuth.credentials.ts
+++ credentials/GreenApiAuth.credentials.ts
@@ -1,8 +1,8 @@
 
 import {
-	ICredentialTestRequest,
 	ICredentialType,
 	INodeProperties,
+	ICredentialTestRequest,
 } from 'n8n-workflow';
 declare const console: any;
 
@@ -33,22 +33,71 @@ export class GreenApiAuth implements ICredentialType {
 		},
 	];
 
-	
+	/*
+	methods = {
+		credentialTest: {
+			async validateConnection(
+				this: ICredentialTestFunctions,
+				credentials: ICredentialDataDecryptedObject,
+			): Promise<INodeCredentialTestResult> {
+				console.log('мы здесь!')
+				try {
+					const response = await this.helpers.request({
+						url: `https://api.green-api.com/waInstance${credentials.idInstance}/getStateInstance/${credentials.apiTokenKey}`,
+						method: 'GET',
+						json: true,
+					});
+
+					if (response.stateInstance !== 'authorized') {
+						return {
+							status: 'Error',
+							message: 'Instance is not authorized',
+						};
+					}
+
+					return {
+						status: 'OK',
+						message: 'Connection successful!',
+					};
+				} catch (error: any) {
+					return {
+						status: 'Error',
+						message: `Authentication failed: ${error.message}`,
+					};
+				}
+			},
+		},
+	};*/
+
 	test: ICredentialTestRequest = {
 		request: {
 			baseURL: 'https://api.green-api.com/',
 			url: '={{$baseURL}}/waInstance{{$credentials.idInstance}}/getStateInstance/{{$credentials.apiTokenKey}}',
 			method: 'GET',
-			returnFullResponse: true,
-			json: true,
 		},
-		rules: [ // ну тут вообще не работает
+		rules: [ // прописать все случаи expired, deleted
 			{
 				type: 'responseSuccessBody',
 				properties: {
-					message: 'wtf',
-					key: 'wtf',
-					value: 'wtf',
+					key: 'stateInstance',
+					value: 'notAuthorized',
+					message: 'Instance is not authorized!',
+				},
+			},
+			{
+				type: 'responseSuccessBody',
+				properties: {
+					key: 'stateInstance',
+					value: 'starting',
+					message: 'Instance is in starting state!',
+				},
+			},
+			{
+				type: 'responseSuccessBody',
+				properties: {
+					key: 'stateInstance',
+					value: 'blocked',
+					message: 'Instance is blocked!',
 				},
 			},
 		],
