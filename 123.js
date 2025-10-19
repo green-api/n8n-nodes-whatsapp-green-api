c552f59ba068514cb1335d45f35001a0eb7610cd
nostalgiaCode <nowelywork@gmail.com>
2025-10-19 14:27:53 +0300

    gitignore

--- 123.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-const buf = fs.readFileSync('fail.jpg'); // замени на своё имя файла
-console.log('Первые 400 байт как текст:\n');
-console.log(buf.slice(0, 400).toString('utf8'));
